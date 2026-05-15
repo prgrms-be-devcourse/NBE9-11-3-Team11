3c87ee6b6e1ff11a188809f749a59bd74544a57c
JunWoo Seo <csjws@naver.com>
2026-05-15 16:10:09 +0900

    feat: BackendApplicationTests 코틀린 마이그레이션 1차

---
+++ backend/src/test/java/com/back/team11/BackendApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.back.team11
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class BackendApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
