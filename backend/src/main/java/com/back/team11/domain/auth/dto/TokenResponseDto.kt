6779d69b13759424df79e788aecb6ad5189ab23b
BroJin <gudwls2777@naver.com>
2026-05-16 17:52:30 +0900

    refactor: auth > dto 코틀린 마이그레이션 2차

--- backend/src/main/java/com/back/team11/domain/auth/dto/TokenResponseDto.kt
+++ backend/src/main/java/com/back/team11/domain/auth/dto/TokenResponseDto.kt
@@ -1,10 +1,6 @@
 package com.back.team11.domain.auth.dto
 
-import lombok.Getter
-import lombok.Setter
 
-@Getter
-@Setter
 class TokenResponseDto(
     private val accessToken: String,
     private val refreshToken: String
