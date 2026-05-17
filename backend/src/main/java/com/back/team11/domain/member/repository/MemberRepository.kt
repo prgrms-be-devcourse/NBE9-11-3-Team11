cc756cbea11a4e1220d424074d8ff92cfd1fa184
BroJin <gudwls2777@naver.com>
2026-05-17 17:58:56 +0900

    refactor: MemberRepository에 findMemberById() 추가

--- backend/src/main/java/com/back/team11/domain/member/repository/MemberRepository.kt
+++ backend/src/main/java/com/back/team11/domain/member/repository/MemberRepository.kt
@@ -3,12 +3,13 @@ package com.back.team11.domain.member.repository
 import com.back.team11.domain.member.entity.Member
 import com.back.team11.domain.member.entity.Provider
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
 
 interface MemberRepository : JpaRepository<Member, Long> {
     fun findByProviderAndProviderId(provider: Provider, providerId: String): Member?
 
     fun findByEmail(email: String): Member?
+
+    fun findMemberById(id: Long): Member?
 
     fun existsByEmail(email: String): Boolean
 }
