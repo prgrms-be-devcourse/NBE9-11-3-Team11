6c15586aeac5841e6e60b03036602f66ef0cb3da
JunWoo Seo <csjws@naver.com>
2026-05-17 16:52:17 +0900

    refactor: review > repository 코틀린 마이그레이션 2차

--- backend/src/main/java/com/back/team11/domain/review/repository/ReviewRepository.kt
+++ backend/src/main/java/com/back/team11/domain/review/repository/ReviewRepository.kt
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import java.util.*
 
 interface ReviewRepository : JpaRepository<Review, Long> {
     fun existsByMemberIdAndCafeId(memberId: Long, cafeId: Long): Boolean //리뷰를 생성한지 안한지 체크
@@ -18,7 +17,7 @@ interface ReviewRepository : JpaRepository<Review, Long> {
     // 페이징 조회
     fun findAllByCafeId(cafeId: Long, pageable: Pageable): Page<Review>
 
-    fun findByIdAndCafeId(reviewId: Long, cafeId: Long): Optional<Review>
+    fun findByIdAndCafeId(reviewId: Long, cafeId: Long): Review?
 
     // @Modifying은 조회가 아닌 데이터 변경(INSERT, UPDATE, DELETE)임을 나타냄
     // clearAutomatically = true는 벌크 연산 후 영속성 컨텍스트를 비워 데이터 불일치를 방지
@@ -29,7 +28,7 @@ interface ReviewRepository : JpaRepository<Review, Long> {
 
     // 전체 조회
     @Query("SELECT r FROM Review r JOIN FETCH r.member JOIN FETCH r.cafe WHERE r.cafe.id = :cafeId ORDER BY r.createdAt DESC")
-    fun findAllByCafeIdWithFetch(@Param("cafeId") cafeId: Long): MutableList<Review>
+    fun findAllByCafeIdWithFetch(@Param("cafeId") cafeId: Long): List<Review>
 
     // 페이징 조회
     @Query("SELECT r FROM Review r JOIN FETCH r.member JOIN FETCH r.cafe WHERE r.cafe.id = :cafeId")
@@ -39,5 +38,5 @@ interface ReviewRepository : JpaRepository<Review, Long> {
     fun findByIdAndCafeIdWithFetch(
         @Param("reviewId") reviewId: Long,
         @Param("cafeId") cafeId: Long
-    ): Optional<Review>
+    ): Review?
 }
