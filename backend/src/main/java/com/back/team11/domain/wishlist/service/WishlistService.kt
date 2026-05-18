df9b6221250054d00b1693fa7690c6d06e3d0699
suuuu9 <asb040923@gmail.com>
2026-05-18 10:15:15 +0900

    fix: WishlistService import 중복 제거

--- backend/src/main/java/com/back/team11/domain/wishlist/service/WishlistService.kt
+++ backend/src/main/java/com/back/team11/domain/wishlist/service/WishlistService.kt
@@ -8,6 +8,7 @@ import com.back.team11.domain.wishlist.repository.WishlistRepository
 import com.back.team11.global.dto.PageResponse
 import com.back.team11.global.exception.CustomException
 import com.back.team11.global.exception.ErrorCode
+import com.back.team11.global.extension.findByIdOrThrow
 import com.back.team11.global.util.AuthUtil
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
