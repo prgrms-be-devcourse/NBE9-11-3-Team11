13ac7630df8b1f40781064a795b303a6e00edcf1
최민규 <153361663+alison0525@users.noreply.github.com>
2026-05-19 14:31:01 +0900

    chore: Docker 및 모니터링 환경 설정 추가 (#37)
    * chore: 백엔드 Dockerfile 추가
    
    * chore: 프론트엔드 Dockerfile 추가
    
    * chore: 로컬 개발환경 docker-compose 및 prometheus 설정 추가
    
    * chore: Prometheus 메트릭 수집을 위한 Actuator 설정 추가
    
    * chore: Actuator 허용 경로 명시적으로 변경 및 일반 JAR 비활성화

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_KAKAO_MAP_KEY: process.env.NEXT_PUBLIC_KAKAO_MAP_KEY,
   },
